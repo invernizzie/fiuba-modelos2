936b1dfc4809bd1b4b7a00a73c13725441937ccc
invernizzie@gmail.com <invernizzie@gmail.com@a51e5f45-78fe-f0b4-433f-b3ab88c1f4c3>
2012-10-06 19:46:46 +0000

    Comienzo el TP 2 con algunos puntos del ejercicio 4

---
+++ tp-stock/watch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LAST_DATE=$(stat -c "%Y" tp-stock.tex)
+
+while true
+do
+	DATE=$(stat -c "%Y" tp-stock.tex)
+	if [ $DATE -gt $LAST_DATE ]
+	then
+		pdflatex tp-stock.tex
+		LAST_DATE="$DATE"
+	fi
+done
